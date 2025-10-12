9e05698ff19e0b5f4912506a122e62bbad373fe8
LevinRobert <levin16robert@gmail.com>
2025-10-12 15:42:21 +0530

    Delete script directory

--- script/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#set -e
-
-##TARGET_DIR="/home/ec2-user/my-python-app"
-
-echo "Cleaning old files"
-##rm -rf "$TARGET_DIR"/*
-rm -rf "/home/ec2-user/my-python-app"/*
-echo "Cleanup complete."
