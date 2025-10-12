cd67ca516a7b1a35569ac5e6d97f1ceda1221af3
LevinRobert <levin16robert@gmail.com>
2025-10-12 16:43:49 +0530

    Update start-app.sh

--- script/start-app.sh
+++ script/start-app.sh
@@ -1,7 +1,4 @@
- #!/bin/bash
-    cd "/home/ec2-user/my-python-app"/
-    # Activate virtual environment if used
-    # source venv/bin/activate
-    pip install -r requirements.txt -t
-    sleep 5s
-    nohup python3 levin.py > logs.txt 2>&1 &
+#!/bin/bash
+cd /home/ec2-user/my-python-app/
+sudo pkill -f levin.py || true
+nohup python3 levin.py > app.log 2>&1 &
