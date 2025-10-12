e87ee78a7a9fdb8db9907664a4c5b01f25fd7191
LevinRobert <levin16robert@gmail.com>
2025-10-12 15:38:13 +0530

    Delete script/app-start.sh

--- script/app-start.sh
+++
@@ -1,7 +0,0 @@
-    #!/bin/bash
-    cd "/home/ec2-user/my-python-app"/
-    # Activate virtual environment if used
-    # source venv/bin/activate
-    pip install -r requirements.txt -t
-    sleep 5s
-    nohup python3 app.py > logs.txt 2>&1 &
